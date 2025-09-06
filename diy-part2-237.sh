84452183d14b1080cb2d95ed58ef8da732a96a1e
jacky shi <102337558+jackyandszy@users.noreply.github.com>
2025-09-06 11:44:55 +0800

    Add files via upload

---
+++ diy-part2-237.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+
+# Modify default theme
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# Modify hostname
+sed -i 's/OpenWrt/jackyandszy/g' package/base-files/files/bin/config_generate
+
+sed -i "s/option start.*/option start \'2\'/g" package/network/services/dnsmasq/files/dhcp.conf
+sed -i "s/option limit.*/option limit \'55\'/g" package/network/services/dnsmasq/files/dhcp.conf
+echo "CONFIG_PACKAGE_luci-app-oaf=y" >>.config
+make defconfig
